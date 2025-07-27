2102950546109c3ec24d954f50b72216745e8089
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-07-27 12:32:08 +0100

    chore: remove unneeded files

--- defer.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Defer subpath entry point
- * Re-exports defer functionality for @xjslang/parser/defer
- */
-
-export * from './src/defer/index';
--- mut.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Mut subpath entry point
- * Re-exports mut functionality for @xjslang/parser/mut
- */
-
-export * from './src/mut/index';
